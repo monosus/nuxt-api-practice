752c1267dbb276921fb7d3f97b625f69645f757b
nono-nop <nopadon@monosus.co.jp>
2023-01-12 18:12:01 +0700

    up: display notices/list

--- pages/notices/list.vue
+++ pages/notices/list.vue
@@ -16,7 +16,7 @@
         </div>
 
         <ul class="c-list-card-post _mt-32">
-          <li v-for="(item, index) in cardList" :key="index">
+          <li v-for="(item, index) in newsList" :key="index">
             <CardPost v-bind="item" />
           </li>
         </ul>
@@ -30,121 +30,13 @@
 
 <script>
 export default {
-  setup() {
-    const cardList = [
-      {
-        link: '#',
-        src: new URL(
-          `@/assets/image/member/common/img_card_post_sp_1.jpg`,
-          import.meta.url
-        ).href,
-        tags: [
-          { style: 'red', text: '重要' },
-          { style: 'black', text: 'ビル' },
-        ],
-        text: '年末年始の営業時間について【H¹T＆提携店含む全店舗】',
-        date: '2022.12.31',
-      },
-      {
-        link: '#',
-        src: new URL(
-          `@/assets/image/member/common/img_card_post_sp_2.jpg`,
-          import.meta.url
-        ).href,
-        tags: [{ style: 'black', text: 'クーポン' }],
-        text: '【10%OFF】YUITO 銀座しゃぶ通 好の笹  日本橋店',
-        date: '2022.12.30',
-      },
-      {
-        link: '#',
-        src: new URL(
-          `@/assets/image/member/common/img_card_post_default_pc.jpg`,
-          import.meta.url
-        ).href,
-        tags: [
-          { style: 'red', text: 'システム' },
-          { style: 'black', text: 'クーポン' },
-        ],
-        text: 'システムメンテナンスのお知らせ',
-        date: '2022.12.28',
-      },
-      {
-        link: '#',
-        src: new URL(
-          `@/assets/image/member/common/img_card_post_sp_4.jpg`,
-          import.meta.url
-        ).href,
-        tags: [{ style: 'black', text: 'イベント・セミナー' }],
-        text: 'THE MODEL 最強の「営業チーム」構築術',
-        date: '2022.12.27',
-      },
-      {
-        link: '#',
-        src: new URL(
-          `@/assets/image/member/common/img_card_post_sp_5.jpg`,
-          import.meta.url
-        ).href,
-        tags: [{ style: 'black', text: 'お知らせ' }],
-        text: '新しくXXXXXXXのサービスが使えるようになりました新しくXXXXXXXのサービスが使えるようになりました新しくXXXXXXXのサービスが使える',
-        date: '2022.12.26',
-      },
-      {
-        link: '#',
-        src: new URL(
-          `@/assets/image/member/common/img_card_post_sp_6.jpg`,
-          import.meta.url
-        ).href,
-        tags: [{ style: 'black', text: 'お知らせ' }],
-        text: '新しくXXXXXXXのサービスが使えるようになりました',
-        date: '2022.12.25',
-      },
-      {
-        link: '#',
-        src: new URL(
-          `@/assets/image/member/common/img_card_post_sp_7.jpg`,
-          import.meta.url
-        ).href,
-        tags: [{ style: 'black', text: 'イベント・セミナー' }],
-        text: 'メータバースやAI・ロボティクスで切り拓くビジネスの未来',
-        date: '2022.12.24',
-      },
-      {
-        link: '#',
-        src: new URL(
-          `@/assets/image/member/common/img_card_post_sp_8.jpg`,
-          import.meta.url
-        ).href,
-        tags: [{ style: 'black', text: 'クーポン' }],
-        text: '【10%OFF】YUITO 銀座しゃぶ通 好の笹  日本橋店',
-        date: '2022.12.23',
-      },
-      {
-        link: '#',
-        src: new URL(
-          `@/assets/image/member/common/img_card_post_sp_1.jpg`,
-          import.meta.url
-        ).href,
-        tags: [
-          { style: 'red', text: '重要' },
-          { style: 'black', text: 'ビル' },
-        ],
-        text: 'エレベーター点検の実施',
-        date: '2022.12.22',
-      },
-      {
-        link: '#',
-        src: new URL(
-          `@/assets/image/member/common/img_card_post_sp_10.jpg`,
-          import.meta.url
-        ).href,
-        tags: [{ style: 'black', text: 'お知らせ' }],
-        text: '新しくXXXXXXXのサービスが使えるようになりました',
-        date: '2022.12.21',
-      },
-    ]
+  async setup() {
+    const { data: newsList } = await useFetch('/api/news', {
+      params: { qty: 5 },
+    })
 
     return {
-      cardList,
+      newsList,
     }
   },
 }
